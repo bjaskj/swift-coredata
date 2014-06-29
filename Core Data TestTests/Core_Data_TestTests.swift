623fd07f79bb6f79cef6aab5d733a31d4d9cfe3d
Bjarte Skjørestad <bjarteskjorestad@Bjartes-Mac.local>
2014-06-29 08:50:20 -0700

    Initial Commit

---
+++ Core Data TestTests/Core_Data_TestTests.swift
@@ -0,0 +1,35 @@
+//
+//  Core_Data_TestTests.swift
+//  Core Data TestTests
+//
+//  Created by Bjarte Skjørestad on 29/06/14.
+//  Copyright (c) 2014 Bjarte Skjørestad. All rights reserved.
+//
+
+import XCTest
+
+class Core_Data_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
